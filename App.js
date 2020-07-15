18c116d9768062de545cc762ceb5d77f01e6d039
Jason Gettel <misternutty@gmail.com>
2020-07-14 17:47:58 -0700

    Initial Navigation

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { ScrollView, Text, StyleSheet, Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+
+export default App = () => {
+  const Tab = createMaterialTopTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Build"
+        style={styles.container}
+      >
+        <Tab.Screen name="Wishlist" component={WishlistPage} />
+        <Tab.Screen name="Build" component={BuildPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
+  }
+})
+
+const WishlistPage = () => {
+  return (
+    <ScrollView>
+      <Text>Wishlist Page</Text>
+    </ScrollView>
+  );
+}
+
+const BuildPage = () => {
+  return (
+    <ScrollView>
+      <Text>Melee Weapon</Text>
+      <Text>Armor</Text>
+      <Text>Ranged Weapon</Text>
+      <Text>Artifact 1</Text>
+      <Text>Artifact 2</Text>
+      <Text>Artifact 3</Text>
+    </ScrollView>
+  );
+}
