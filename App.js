8bc80c562d66a6ee826e86ae1dedf88709a75e50
Jason Gettel <misternutty@gmail.com>
2020-09-17 16:47:32 -0700

    Base BuildPage

--- App.js
+++ App.js
@@ -2,48 +2,32 @@ import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { ScrollView, Text, StyleSheet, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-
+import { BuildPage } from './pages/BuildPage';
 
 export default App = () => {
-  const Tab = createMaterialTopTabNavigator();
+    const Tab = createMaterialTopTabNavigator();
 
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Build"
-        style={styles.container}
-      >
-        <Tab.Screen name="Wishlist" component={WishlistPage} />
-        <Tab.Screen name="Build" component={BuildPage} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
+    return (
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName="Build" style={styles.container}>
+                <Tab.Screen name="Wishlist" component={WishlistPage} />
+                <Tab.Screen name="Build" component={BuildPage} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
-  }
-})
+    container: {
+        flex: 1,
+        paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
+    },
+});
 
 const WishlistPage = () => {
-  return (
-    <ScrollView>
-      <Text>Wishlist Page</Text>
-    </ScrollView>
-  );
-}
-
-const BuildPage = () => {
-  return (
-    <ScrollView>
-      <Text>Melee Weapon</Text>
-      <Text>Armor</Text>
-      <Text>Ranged Weapon</Text>
-      <Text>Artifact 1</Text>
-      <Text>Artifact 2</Text>
-      <Text>Artifact 3</Text>
-    </ScrollView>
-  );
-}
+    return (
+        <ScrollView>
+            <Text>Wishlist Page</Text>
+        </ScrollView>
+    );
+};
---
+++ pages/BuildPage.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { ScrollView, Text, Image, StyleSheet, View } from 'react-native';
+import { CheckBox, Divider } from 'react-native-elements';
+
+export const BuildPage = () => {
+    return (
+        <ScrollView style={styles.container}>
+            <Text style={styles.label}>Melee Weapon</Text>
+            <Item />
+            <Divider style={styles.divider} />
+            <Text style={styles.label}>Armor</Text>
+            <Item />
+            <Divider style={styles.divider} />
+            <Text style={styles.label}>Ranged Weapon</Text>
+            <Item />
+            <Divider style={styles.divider} />
+            <Text style={styles.label}>Artifact 1</Text>
+            <Item />
+            <Divider style={styles.divider} />
+            <Text style={styles.label}>Artifact 2</Text>
+            <Item />
+            <Divider style={styles.divider} />
+            <Text style={styles.label}>Artifact 3</Text>
+            <Item />
+            <Divider style={styles.divider} />
+        </ScrollView>
+    );
+};
+
+const Item = () => {
+    const [checked, setChecked] = React.useState(false);
+
+    return (
+        <View style={styles.item}>
+            <Image source={{ uri: '' }} style={styles.itemImage} />
+            <View style={styles.itemSummary}>
+                <Text style={styles.itemName}>Item Name</Text>
+                <Text style={styles.itemModifier}>o Modifier 1</Text>
+                <Text style={styles.itemModifier}>o Modifier 2</Text>
+                <Text style={styles.itemModifier}>o Modifier 3</Text>
+                <CheckBox
+                    title="Wishlist"
+                    uncheckedIcon="plus"
+                    checkedIcon="check"
+                    checkedColor="green"
+                    checked={checked}
+                    onPress={() => setChecked(!checked)}
+                />
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 15,
+    },
+    label: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    divider: {
+        height: 3,
+        marginVertical: 5,
+    },
+    item: {
+        flexDirection: 'row',
+        marginBottom: 5,
+    },
+    itemImage: {
+        width: 150,
+        height: 150,
+        backgroundColor: '#999',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+    },
+    itemSummary: {
+        marginLeft: 15,
+    },
+    itemName: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    itemModifier: {
+        fontSize: 14,
+    },
+});
