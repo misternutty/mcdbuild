fda149f307584adc06fd57c5e8563a8d23d4473b
Jason Gettel <misternutty@gmail.com>
2020-09-25 17:24:18 -0700

    Manipulate build with SearchableList

--- pages/BuildPage.js
+++ pages/BuildPage.js
@@ -1,44 +1,92 @@
 import React from 'react';
-import { ScrollView, Text, Image, StyleSheet, View } from 'react-native';
-import { CheckBox, Divider } from 'react-native-elements';
-import { baseUrl } from '../shared/baseUrl';
+import { ScrollView, Image, Text, StyleSheet, View } from 'react-native';
+import { CheckBox, Divider, Overlay } from 'react-native-elements';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+// import { baseUrl } from '../shared/baseUrl';
+import { SearchableList } from '../components/SearchableList';
 
 export const BuildPage = () => {
+    const [build, setBuild] = React.useState({
+        melee: 'Melee Weapon',
+        armor: 'Armor',
+        ranged: 'Ranged Weapon',
+        artifactOne: 'Artifact 1',
+        artifactTwo: 'Artifact 2',
+        artifactThree: 'Artifact 3',
+    });
+
+    const setItem = (slot, name) => {
+        // takes an item slot and the name of the new item
+        let newBuild = { ...build, [slot]: name }; // spread the build and replace the item name for the given slot
+        setBuild(newBuild);
+        console.log(newBuild);
+    };
+
     return (
         <ScrollView style={styles.container}>
             <Text style={styles.label}>Melee Weapon</Text>
-            <Item />
+            <Item type="melee" name={build.melee} onChange={setItem} />
             <Divider style={styles.divider} />
             <Text style={styles.label}>Armor</Text>
-            <Item />
+            <Item type="armor" name={build.armor} onChange={setItem} />
             <Divider style={styles.divider} />
             <Text style={styles.label}>Ranged Weapon</Text>
-            <Item />
+            <Item type="ranged" name={build.ranged} onChange={setItem} />
             <Divider style={styles.divider} />
             <Text style={styles.label}>Artifact 1</Text>
-            <Item />
+            <Item
+                type="artifactOne"
+                name={build.artifactOne}
+                onChange={setItem}
+            />
             <Divider style={styles.divider} />
             <Text style={styles.label}>Artifact 2</Text>
-            <Item />
+            <Item
+                type="artifactTwo"
+                name={build.artifactTwo}
+                onChange={setItem}
+            />
             <Divider style={styles.divider} />
             <Text style={styles.label}>Artifact 3</Text>
-            <Item />
+            <Item
+                type="artifactThree"
+                name={build.artifactThree}
+                onChange={setItem}
+            />
             <Divider style={styles.divider} />
         </ScrollView>
     );
 };
 
-const Item = () => {
+const Item = (props) => {
     const [checked, setChecked] = React.useState(false);
+    const [overlayVisible, setOverlayVisible] = React.useState(false);
+
+    const toggleOverlay = () => {
+        setOverlayVisible(!overlayVisible);
+    };
+
+    const dataArray = [
+        { id: 0, name: 'Sword' },
+        { id: 1, name: 'Axe' },
+        { id: 2, name: 'Spear' },
+    ];
+
+    const setThisItem = (item) => {
+        props.onChange(props.type, item.name);
+        toggleOverlay();
+    };
 
     return (
         <View style={styles.item}>
-            <Image
-                source={require('../assets/item-default.png')}
-                style={styles.itemImage}
-            />
+            <TouchableOpacity onPress={toggleOverlay}>
+                <Image
+                    source={require('../assets/item-default.png')}
+                    style={styles.itemImage}
+                />
+            </TouchableOpacity>
             <View style={styles.itemSummary}>
-                <Text style={styles.itemName}>Item Name</Text>
+                <Text style={styles.itemName}>{props.name}</Text>
                 <Text style={styles.itemModifier}>o Modifier 1</Text>
                 <Text style={styles.itemModifier}>o Modifier 2</Text>
                 <Text style={styles.itemModifier}>o Modifier 3</Text>
@@ -51,6 +99,9 @@ const Item = () => {
                     onPress={() => setChecked(!checked)}
                 />
             </View>
+            <Overlay isVisible={overlayVisible} onBackdropPress={toggleOverlay}>
+                <SearchableList data={dataArray} onSelection={setThisItem} />
+            </Overlay>
         </View>
     );
 };
